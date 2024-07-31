37c6c018e333bbe354e90b44298e29dfb531e562
Pavel Smolkov <smolcov@bk.ru>
2024-08-01 01:07:13 +0700

    fixed example.py

--- example.py
+++ example.py
@@ -2,10 +2,5 @@
 
 txt_arr = ['I', 'love', 'Git', 'so much', 'every day']
 txt = ' '.join(txt_arr) + '!'
-<<<<<<< HEAD
+print('Execution finished')
 print('Done: ' + txt)
-print('Execution finished')
-=======
-print('Execution finished')
-print('Result: ' + txt)
->>>>>>> dev
